c5ff2f9a10ae63901cd6ff212dd183ec49c7da54
alexmickelson <alexmickelson96@gmail.com>
2019-08-28 21:20:38 -0600

    Add project files.

---
+++ kubectlWrapper.Shared/ViewModels/KubectlViewModel.cs
@@ -0,0 +1,59 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace kubectlWrapper.Shared.ViewModels
+{
+    public class KubectlViewModel : ViewModelBase
+    {
+        private string nodes;
+        private string output;
+        private string error;
+
+        public string Nodes
+        {
+            get { return nodes; }
+            set { nodes = value; }
+        }
+
+
+        public string Output
+        {
+            get => output;
+            set
+            {
+                output = value;
+                RaisePropertyChanged(nameof(Output));
+            }
+        }
+        public string Error
+        {
+            get => error;
+            set
+            {
+                error = value;
+                RaisePropertyChanged(nameof(Error));
+            }
+        }
+
+        public async void GetNodes()
+        {
+            var p = new Process();
+            p.StartInfo.FileName = "ssh";
+
+            p.StartInfo.Arguments = "benny kubectl get nodes";
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.Start();
+
+            //2) capture stdout
+            Output = String.Empty;
+            Error = String.Empty;
+
+            p.WaitForExit();
+        }
+    }
+}
---
+++ kubectlWrapper.Tests/kubectlViewModelUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using kubectlWrapper.Shared.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kubectlWrapper.Tests
+{
+    class kubectlViewModelUnitTests
+    {
+
+        [Test]
+        public void kubectl_gets_nodes()
+        {
+            var vm = new KubectlViewModel();
+            vm.Output = string.Empty;
+            vm.GetNodes();
+
+            vm.Output.Should().NotBeNullOrEmpty();
+
+        }
+    }
+}
